4bf91f3223b29f8919bb4de2b29852ff8390a70b
Joonkyu Jang <2021193003@yonsei.ac.kr>
2022-07-14 08:09:05 +0200

    changed replace logic to preserve double newline

--- dad.py
+++ dad.py
@@ -10,13 +10,11 @@ left_column, right_column = st.columns(2)
 with left_column:
     st.text_area("입력 텍스트", height=512, key="text")
 
-edited_text = st.session_state.text.replace("\n", "")
+edited_text = st.session_state.text.replace("\n\n", "#뜀#").replace("\n", "").replace("#뜀#", "\n\n")
 
 with right_column:
     st.write("출력 텍스트")
     st.write(edited_text)
-    # if st.button('복사하기'):
-    #     pyperclip.copy(edited_text)
 
     copy_button = Button(label="복사하기")
     copy_button.js_on_event("button_click", CustomJS(args=dict(edited_text=edited_text), code="""
