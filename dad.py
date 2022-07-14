1ce3e236533d14849d3e1da105a7545cbf962546
Joonkyu Jang <2021193003@yonsei.ac.kr>
2022-07-14 08:18:07 +0200

    changed replace double newline to single newline

--- dad.py
+++ dad.py
@@ -10,7 +10,7 @@ left_column, right_column = st.columns(2)
 with left_column:
     st.text_area("입력 텍스트", height=512, key="text")
 
-edited_text = st.session_state.text.replace("\n\n", "#뜀#").replace("\n", "").replace("#뜀#", "\n\n")
+edited_text = st.session_state.text.replace("\n\n", "#뜀#").replace("\n", "").replace("#뜀#", "\n")
 
 with right_column:
     st.write("출력 텍스트")
