053bb3575ed4ee0967c1a7bc9186a57e09333632
Joonkyu Jang <2021193003@yonsei.ac.kr>
2024-10-08 16:25:58 +0900

    fixed copy button with st-copy-to-clipboard

--- dad.py
+++ dad.py
@@ -1,6 +1,6 @@
 import re
 import streamlit as st
-import pyperclip
+from st_copy_to_clipboard import st_copy_to_clipboard
 
 st.title("아빠를 위한 줄바꿈 캐릭터 없애기")
 
@@ -9,14 +9,13 @@ left_column, right_column = st.columns(2)
 with left_column:
     st.text_area("입력 텍스트", height=512, key="text")
 
-substituted_text = re.sub("\n(\s*\n+\s*)+", "<뜀>", st.session_state.text)
-replaced_text = re.sub("\s*\n+\s*", " ", substituted_text)
+substituted_text = re.sub("\n(\\*\n+\\*)+", "<뜀>", st.session_state.text)
+replaced_text = re.sub("\\*\n+\\*", " ", substituted_text)
 edited_text = replaced_text.replace("<뜀>", "\n\n")
 
 with right_column:
     st.write("출력 텍스트")
     st.markdown(edited_text)
-
-    if st.button("복사하기"):
-        pyperclip.copy(edited_text)
-        st.success("복사완료!")
+    st_copy_to_clipboard(
+        edited_text, before_copy_label="복사하기", after_copy_label="복사완료"
+    )
